3189b661d9c902b9e5eec0e5a0ed4c42a1e053db
Ana Rocha <anasoledad.rocha@gmail.com>
2023-05-20 23:05:19 -0300

    Se agregó los componentes ItemTarea y ListaTareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Formulario from './components/Formulario';
 
 function App() {
 
@@ -11,7 +12,9 @@ function App() {
         <h1 className='display-4 text-center'>Lista de Tareas</h1>
         <hr/>
         {/* aqui agrego el formulario */}
+        <Formulario></Formulario>
       </Container>
+      
       <footer className='bg-dark text-light text-center py-4'>
         <p>&copy; Todos los derechos reservados</p>
       </footer>
---
+++ src/components/ItemTarea.jsx
@@ -0,0 +1,9 @@
+import { ListGroup } from "react-bootstrap";
+
+const ItemTarea = () => {
+    return (
+        <ListGroup.Item></ListGroup.Item>
+    );
+};
+
+export default ItemTarea;
