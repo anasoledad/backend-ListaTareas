43d3303d6ddf50a157e5c720f9151bb339dd7980
Ana Rocha <anasoledad.rocha@gmail.com>
2023-05-21 11:43:46 -0300

    Se agregó el state tarea y lo controlamos con el evento change

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,12 +1,18 @@
 import {Form, Button, ListGroup} from 'react-bootstrap'
 import ListaTareas from './ListaTareas';
+import { useState } from 'react';
 
 const Formulario = () => {
+  const [tarea, setTarea] = useState('');
+  const [tareas, setTareas] = useState([]);
+
   return (
     <section>
       <Form>
         <Form.Group className="mb-3 d-flex" controlId="tarea">
-          <Form.Control type="texto" placeholder="Ingrese una tarea" />
+          <Form.Control type="texto" placeholder="Ingrese una tarea" onChange={(e)=> setTarea(e.target.value)}
+          value={tarea}
+          />
           <Button variant="primary" type="submit">
           Enviar
           </Button>
